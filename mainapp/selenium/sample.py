0252e9db2045c88c562863306806bbb05dfc4cf1
Kasinath J <jrkasinath@gmail.com>
2023-02-15 22:08:14 +0530

    Update sample.py

--- mainapp/selenium/sample.py
+++ mainapp/selenium/sample.py
@@ -10,10 +10,10 @@ from bs4 import BeautifulSoup
 
 # from webdriver_manager.chrome import ChromeDriverManager
 
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach",True)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach",True)
 
-# serv_obj = Service("backend\selenium_test\chromedriver.exe")
+serv_obj = Service("backend\selenium_test\chromedriver.exe")
 
 def linkedin_scratch(profile):
     
